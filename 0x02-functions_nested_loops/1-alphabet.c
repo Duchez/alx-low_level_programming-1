5741f08cff86236d1fa51b44f2a1a0935d6e5f4d
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 03:51:12 +0300

    prints alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,18 +14,6 @@ void print_alphabet(void)
 		_putchar(alphabets);
 	}
 	_putchar('\n');
-}
 
-/**
-* main- calls the function print_alphabet
-*
-* Return: ALways 0 (sucess)
-*/
-
-int main(void)
-
-{
-print_alphabet();
-
-return (0);
+	return (0);
 }
