3c93fe71fb475c183038111b348d2844e38a6e46
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 18:34:46 +0300

    Negative or Positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-# include "holberton.h"
+#include "holberton.h"
 
 /**
- * main - entry point. Tests function for integer sign
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
+
 	i = 0;
-
-
 	positive_or_negative(i);
 
 	return (0);
