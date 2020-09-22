8b6fd0db9009754cd5833873fed7e058fbfb3c07
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 14:59:21 +0300

    Addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+# include "holberton.h"
+
+/**
+ * add_int() - prints the result of addition of two numbers
+ *
+ * Return - Always an int (Success)
+ */
+
+int add(int first_number, int second_number)
+{
+	return (first_number + second_number)
+}
