647408104712e47e7b71e1fec2056ba17abd30b6
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 06:24:28 +0300

    24:00 Hour Countdown

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: Alwas 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+}
