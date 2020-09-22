9b9b6ebf774136a64af47bf8ec5dfce44a2e01f9
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 15:26:01 +0300

    Prints Natural Numbers from entry point to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,39 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - print all natural numbers from n to 98.
- * @n: Integer argument to be passed
- * Return: Returns nothing.
+ * print_to_98 - Printing natural numbers from number to 98.
+ * @number: The first counter from to 98
+ * Return: void returns nothing.
  */
 
-void print_to_98(int n)
+void print_to_98(int number)
 {
-	if (n <= 98)
+	if (number < 98)
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			n++;
-
-		}
-		putchar('\n');
-	}
+	for (number = number; number < 98; number++)
+	printf("%d, ", number);
+	printf("%d\n", 98);
+	}	
 	else
 	{
-		for (; n > 98; n--)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		putchar(''\n');
+	for (number = number; number > 98; number--)
+	printf("%d, ", number);
+	printf("%d\n", 98);
 	}
 }
