d89d2f92c1629998516b3ec7a02aba7809c99111
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 04:38:05 +0300

    print the alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 - 10 times alphabet, in lowercase with
+*
+* Return: Always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int count;
+	char alphabets;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (alphabets = 97; alphabets <= 122; alphabets++)
+		{
+			_putchar(alphabets);
+		}
+		_putchar('\n');
+	}
+}
+
+/**
+* main - Calls rint_alphabet_x10()
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
