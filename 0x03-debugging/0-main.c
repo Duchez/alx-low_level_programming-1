dadded817ebdacd8450e5b192fb19ba4d532e59d
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-22 17:44:03 +0300

    Test for integer sign

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+# include "holberton.h"
+
+/**
+ * main - entry point. Tests function for integer sign
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int test;
+
+	test = 0;
+
+	positive_or_negative(test);
+
+	return (0);
+}
