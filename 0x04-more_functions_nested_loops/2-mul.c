e6bae22bf23ed1141b3add9ec623bca27193a100
Bryan Alango <b.alango@launchpad.alx.app>
2020-09-24 10:36:30 +0300

    Multiplies Two Integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+# include "holberton.h"
+
+/**
+ * _mul - Entry point. Multiplies two integers
+ * @a: first integer value to be multiplied
+ * @b: second integer value to be multiplied
+ * Return: Returns the result of multiplied a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
